548c349dcb85f8ad7f7b04a532345b6ce169f8bd
WeiChun12346 <97076415+WeiChun12346@users.noreply.github.com>
2024-02-05 04:28:31 +0800

    .gitignore is now working

--- index.js
+++ index.js
@@ -3,11 +3,13 @@ import middleware from './config/middleware.js';
 import routes from './routes/index.js';
 import connectToDatabase from './config/db.js';
 import bodyParser from 'body-parser'
+import cors from 'cors';
 
 const app = express();
 const port = process.env.PORT;
 
 const startServer = async () => {
+    app.use(cors());
     await connectToDatabase();
     app.use(middleware)
     app.use(bodyParser.json());
